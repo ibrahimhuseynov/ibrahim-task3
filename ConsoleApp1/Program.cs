4650441f6a89cb638de67d9b478c57b7bf1af17a
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-10 09:48:06 +0400

    aztu proekt

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(number(5));
+            Console.ReadLine();
+        }
+       
+        static int number(int num)
+        {
+            int i = 0;int s=0;
+            while (s<=num)
+            {
+                i++;
+                s = i * i;
+            }
+            return (i-1);
+        }
+    }
+}
